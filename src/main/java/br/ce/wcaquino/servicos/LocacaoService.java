bdbe69f6f1e8d3a1d0eb55d06d57fa0fdfa132d7
odevpedro <pedrosschmidt2@gmail.com>
2024-01-30 10:57:56 -0300

    test: asserts objetos

--- src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
+++ src/main/java/br/ce/wcaquino/servicos/LocacaoService.java
@@ -32,20 +32,5 @@ public class LocacaoService {
 		return locacao;
 	}
 
-	@Test
-	public  void teste() {
-		//declarar cenários, inicializar variaveis
-		LocacaoService service = new LocacaoService();
-		Usuario usuario = new Usuario("Usuário 1");
-		Filme filme = new Filme("Filme 2", 2, 5.0);
 
-		//ação invocar o método que queremos testar
-		Locacao locacao = service.alugarFilme(usuario, filme);
-
-		//verificação, assertivas
-		Assert.assertTrue(locacao.getValor() == 5.0);
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataLocacao(), new Date()));
-		Assert.assertTrue(DataUtils.isMesmaData(locacao.getDataRetorno(), DataUtils.obterDataComDiferencaDias(1)));
-		
-	}
 }
